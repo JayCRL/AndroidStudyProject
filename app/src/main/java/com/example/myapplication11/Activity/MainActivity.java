3aaace3148a4661124fa133dcc763efb7899fd93
JayCRL <2556693598.com>
2025-10-10 22:33:25 +0800

    Study the NavBottomBar and make the list more compelete

--- app/src/main/java/com/example/myapplication11/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication11/Activity/MainActivity.java
@@ -2,7 +2,17 @@ package com.example.myapplication11.Activity;// 导入必要的包
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import com.example.myapplication11.R;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 // 继承 AppCompatActivity
 public class MainActivity extends AppCompatActivity {
     // Activity 创建时调用（生命周期第一步）
@@ -11,6 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+        //getFragmentManager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //get NavHostFragment
+        NavHostFragment navHostFragment = (NavHostFragment) fragmentManager.findFragmentById(R.id.fragmentContainerView3);
+        //get NavController
+        NavController navController = navHostFragment.getNavController();
+        //configure the AppBar
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.firstFragment2,
+                R.id.secondFragment2,
+                R.id.thirdFragment2
+        ).build();
+        //set the configure
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
     //启动
     @Override
